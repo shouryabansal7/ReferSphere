d66a3458761d729ac5c807779887054c0dbe2211
Shourya Bansal <shouryab2001@gmail.com>
2024-11-02 03:29:30 +0530

    first commit

---
+++ src/main/java/com/springproject/refersphere/RefersphereApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.refersphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefersphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefersphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproject/refersphere/RefersphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.refersphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefersphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
